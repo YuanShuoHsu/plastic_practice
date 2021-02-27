4bda5b4e02a75d35dfe42a614a6f07e5ca8e169f
許元碩 <79447837+YuanShuoHsu@users.noreply.github.com>
2021-02-28 01:21:29 +0800

    Update data_transfer.js

--- data_transfer.js
+++ data_transfer.js
@@ -1,16 +1,8 @@
 function keyboard_enter_dropdown(event){
     if (event.key == "Enter"){ 
-        if (typeof(Storage) !== "undefined"){
-            // Store
-            localStorage.setItem("storage_text", document.getElementById("search_text_dropdown").value);
-            // Retrieve
-            document.getElementById("information").innerHTML = localStorage.getItem("storage_text");
-        }
-        else{
-            document.getElementById("information").innerHTML = "Sorry, your browser does not support Web Storage...";
-        }
-        // event.preventDefault();
-        // document.getElementById("information").innerHTML = document.getElementById("search_text_dropdown").value;
+        event.preventDefault();
+        document.getElementById("information").innerHTML = document.getElementById("search_text_dropdown").value;
+        localStorage.setItem("storage_text", document.getElementById("search_text_dropdown").value);
     }
 }
 
@@ -22,15 +14,10 @@ function keyboard_enter_collapse(event){
 }
 
 function search_data(){
-    if (typeof(Storage) !== "undefined"){
-        // Store
-        localStorage.setItem("storage_text", document.getElementById("search_text_collapse").value);
-        // Retrieve
-        document.getElementById("information").innerHTML = localStorage.getItem("storage_text");
-    }
-    else{
-        document.getElementById("information").innerHTML = "Sorry, your browser does not support Web Storage...";
-    }
-    // document.getElementById("information").innerHTML = document.getElementById("search_text_collapse").value;
+    document.getElementById("information").innerHTML = document.getElementById("search_text_collapse").value;
+    localStorage.setItem("storage_text", document.getElementById("search_text_collapse").value);
+}
 
+function load_local_storage(){
+    document.getElementById("information").innerHTML = localStorage.getItem("storage_text");
 }
