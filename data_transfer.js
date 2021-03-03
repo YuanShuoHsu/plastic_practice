8025f5931b5dede14299b3b42d73433c8a4611e6
許元碩 <79447837+YuanShuoHsu@users.noreply.github.com>
2021-03-04 04:37:43 +0800

    Update data_transfer.js

--- data_transfer.js
+++ data_transfer.js
@@ -1,18 +1,22 @@
+
 function keyboard_enter_dropdown(event){
-    if (event.key == "Enter"){ 
+    if (event.key == "Enter"){
         event.preventDefault();
-        document.getElementById("information").innerHTML = document.getElementById("search_text_dropdown").value;
-        localStorage.setItem("storage_text", document.getElementById("search_text_dropdown").value);
+        inputform = 0;
+        document.getElementById("hidden_button").click();
     }
+}
+function search_data_hidden(){
+    document.getElementById("information").innerHTML = document.getElementById("search_text_dropdown").value;
+    localStorage.setItem("storage_text", document.getElementById("search_text_dropdown").value);
 }
 
 function keyboard_enter_collapse(event){
-    if (event.key == "Enter"){ 
+    if (event.key == "Enter"){
         event.preventDefault();
         document.getElementById("search_button").click();
     }
 }
-
 function search_data(){
     document.getElementById("information").innerHTML = document.getElementById("search_text_collapse").value;
     localStorage.setItem("storage_text", document.getElementById("search_text_collapse").value);
